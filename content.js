1816bcc4879bd15183d5a8174ab5941eac791d52
erenhncr <erenhncr@gmail.com>
2020-03-08 16:27:18 +0300

    bugs fixed

--- content.js
+++ content.js
@@ -1,36 +1,38 @@
+// main includes all cards.
 let main = document.getElementsByClassName('tw-pd-b-3 tw-pd-t-2 tw-pd-x-3')[0];
-
 
 const config = { attributes: true, childList: true, subtree: true };
 
 const callback = function(mutationsList, observer) {
-  console.log('gönderdim');
+  console.log(x + ' channel was removed');
   checkGames();   
 }
+// if dom changed
 const observer = new MutationObserver(callback);
 
 observer.observe(main, config);
 
-
-function removeGames(x=0) {
+let x = 0;
+function removeGames() {
   let cards = main.getElementsByClassName('preview-card');
-
+  const conditions = ['league of legends',`playerunknown's battlegrounds`, 'counter-strike: global offensive', 'pubg mobile', 'grand theft auto v',
+  'fortnite', 'zula', 'knight online', 'dota 2', 'teamfight tactics', 'slots', 'metin 2', 'black desert online', 'hearthstone', ,];
   for (let i = 0; i < cards.length; i++) {
-    // if match the game name
+    
     try {
+      // if the card has child
       if(cards[i].parentElement.parentElement.parentElement.parentElement.parentElement.hasChildNodes){
-        if(cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase() == 'league of legends' ||
-          cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase() == 'counter-strike: global offensive' ||
-          cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase() == `playerunknown's battlegrounds` ||
-          cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase() == `pubg mobile` ||
-          cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase() == `grand theft auto v` ||
-          cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase() == `fortnite`){
-
-            console.log(cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase());
-            cards[i].parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(cards[i].parentElement.parentElement.parentElement.parentElement); 
-          }
+        // if match the game name
+        if(conditions.some(el => cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase().includes(el))){
+          // how many cards are deleted?
+          x++;
+          console.log(cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase());
+          // delete 4 upper parent
+          cards[i].parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(cards[i].parentElement.parentElement.parentElement.parentElement); 
         }
-      } catch (error) {}
+       
+      }
+    } catch (error) {}
   }
 }
 
