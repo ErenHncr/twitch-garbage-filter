f357d3bb02f30e777d8b4a26d969f3a7c13d7b5f
erenhncr <erenhncr@gmail.com>
2020-03-08 16:48:04 +0300

    condition variable changed to bannedGames

--- content.js
+++ content.js
@@ -15,20 +15,27 @@ observer.observe(main, config);
 let x = 0;
 function removeGames() {
   let cards = main.getElementsByClassName('preview-card');
-  const conditions = ['league of legends',`playerunknown's battlegrounds`, 'counter-strike: global offensive', 'pubg mobile', 'grand theft auto v',
-  'fortnite', 'zula', 'knight online', 'dota 2', 'teamfight tactics', 'slots', 'metin 2', 'black desert online', 'hearthstone', ,];
+  const bannedGames = ['league of legends',`playerunknown's battlegrounds`, 'counter-strike: global offensive', 'pubg mobile', 'grand theft auto v',
+  'fortnite', 'zula', 'knight online', 'dota 2', 'teamfight tactics', 'slots', 'metin 2', 'black desert online', 'hearthstone'];
+
+  
+
   for (let i = 0; i < cards.length; i++) {
     
     try {
       // if the card has child
       if(cards[i].parentElement.parentElement.parentElement.parentElement.parentElement.hasChildNodes){
         // if match the game name
-        if(conditions.some(el => cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase().includes(el))){
+        if(bannedGames.some(el => cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase().includes(el))){
           // how many cards are deleted?
           x++;
           console.log(cards[i].children[1].children[1].children[0].children[1].children[1].children[0].children[0].textContent.toLowerCase());
           // delete 4 upper parent
           cards[i].parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(cards[i].parentElement.parentElement.parentElement.parentElement); 
+        }
+        else {
+          // get channel names
+          console.log(cards[17].children[1].children[1].children[0].children[1].children[0].children[0].children[0].textContent);
         }
        
       }
